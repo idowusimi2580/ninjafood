17d67eb4ccf6249d9f5bdea3d818d6890d721beb
Similoluwa <idowusimi8@gmail.com>
2022-12-20 13:21:24 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["C:\Users\ADERAYO\Videos\tailwind\ninjafood\src\style.css"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
